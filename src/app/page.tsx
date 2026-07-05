e6c3dcdca75a3a36d1da569abc68c4a73189b8bc
Eduardovazquez1881 <eduardovz1881@gmail.com>
2026-07-04 20:04:05 -0700

    cambios en la url, se elimino la ruta portafolio y se puso en la base del proyecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,78 @@
+"use client"
+import React from "react";
+import Navegation from "@/components/ui/navegation";
+import Typewriter from "@/components/animations/typewrite";
 import Image from "next/image";
+import { motion } from "framer-motion"
+import Linkedin from "@/components/icons/linkedin";
 
-export default function Home() {
+const Page = () => {
+
+  const descargarCV = () => {
+    const link = document.createElement('a');
+    link.href = '/pdf/cv.pdf';
+    link.download = 'Eduardo_Vazquez_CV.pdf';
+    link.click();
+  }
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="">
+      <Navegation />
+      <div className="h-screen mx-4 flex flex-col md:mx-5 lg:mx-45 md:flex-row">
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="p-4 mt-30 lg:ml-50 space-y-5 md:w-1/2 lg:space-y-8">
+
+          <h1 className="font-bold font-sans w-150 text-3xl md:text-4xl lg:text-6xl lg:leading-19">
+            Hola soy{" "}
+            <span className="bg-gradient-to-r from-amber-500 to-orange-600 bg-clip-text text-transparent">
+              Eduardo Vazquez
+            </span>
+          </h1>
+          <Typewriter text="Developer fullstack" className="font-sans font-semibold text-xl md:text-2xl lg:text-4xl" />
+          <h3 className="mt-4 text-left font-stretch-normal leading-10 text-lg xl:w-150 md:text-xl lg:mt-7">Apasionado por el desarrollo de software, con experiencia en proyectos academicos y personales que abarcan frontend, backend
+            y programacion de sistemas</h3>
+
+          <div className="flex">
+            <motion.button
+              onClick={descargarCV}
+              whileHover={{
+                y: -10,
+                scale: 1.05,
+              }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+              className="py-3 px-6 rounded-xl bg-gradient-to-r from-amber-500 to-orange-600 text-white font-semibold shadow-lg hover:shadow-xl transition-shadow"
+            >
+              Descargar CV
+            </motion.button>
+
+            <a href="https://www.linkedin.com/in/eduardo-vazquez-0aaa5b242/">
+              <Linkedin className="text-blue-900" size={40} />
+            </a>
+          </div>
+
+        </div>
+
+
+        <div className="md:w-1/2 text-black mt-30">
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            whileHover={{ scale: 1.05, rotate: 2 }}
+            transition={{ duration: 0.5, type: "spring", stiffness: 300 }}
           >
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="rounded-full shadow-2xl"
+              src="/image/image.png"
+              alt="Profile image"
+              width={400}
+              height={400}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </motion.div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      </div>
     </div>
   );
-}
+};
+
+export default Page;
